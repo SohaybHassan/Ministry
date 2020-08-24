c6c0f8c235c2af4b545c2a7ac77371cb07690a9b
sohiyp199716 <sohiyp199716@gmail.com>
2020-08-24 11:40:12 +0300

    تقديم شكوى عملية

--- app/src/main/java/com/sh/wm/ministry/featuers/home/homeFiles/workercompilation/view/WorkerCompilationFormOneFragment.java
+++ app/src/main/java/com/sh/wm/ministry/featuers/home/homeFiles/workercompilation/view/WorkerCompilationFormOneFragment.java
@@ -2,7 +2,10 @@ package com.sh.wm.ministry.featuers.home.homeFiles.workercompilation.view;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +17,9 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.sh.wm.ministry.custem.BottomSheetSearsh;
+import com.sh.wm.ministry.custem.BottomSheetSearshList;
 import com.sh.wm.ministry.databinding.FragmentFileComplaintForm1Binding;
 import com.sh.wm.ministry.featuers.home.OnFragmentInteractionListener;
 import com.sh.wm.ministry.featuers.home.homeFiles.workercompilation.model.ConstructByName;
@@ -26,19 +32,19 @@ public class WorkerCompilationFormOneFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
     private WorkerCompilationFormTwoViewModel workerCompilationFormTwoViewModel;
     private Observer<ConstructByName> constructByNameObserver;
-private static final String TAG= WorkerCompilationFormOneFragment.class.getSimpleName();
+    private BottomSheetSearshList bottomSheetSearshList;
+    private BottomSheetDialog bottomSheetDialog;
+    private static final String TAG = WorkerCompilationFormOneFragment.class.getSimpleName();
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        constructByNameObserver = new Observer<ConstructByName>() {
-            @Override
-            public void onChanged(ConstructByName constructByName) {
-                if (constructByName != null) {
-                   Log.d(TAG, "onChanged: "+   constructByName.getConstructs().get(1));;
-                } else {
-                    Log.d(TAG, "onChanged: no data");
-                    Toast.makeText(getActivity(), "no data", Toast.LENGTH_SHORT).show();
-                }
+        constructByNameObserver = constructByName -> {
+            if (constructByName != null) {
+                Log.d(TAG, "onChanged: " + constructByName.getConstructs().get(1));
+            } else {
+                Log.d(TAG, "onChanged: no data");
+                Toast.makeText(getActivity(), "no data", Toast.LENGTH_SHORT).show();
             }
         };
 
@@ -49,7 +55,7 @@ private static final String TAG= WorkerCompilationFormOneFragment.class.getSimpl
                              Bundle savedInstanceState) {
         binding = FragmentFileComplaintForm1Binding.inflate(inflater, container, false);
         View view = binding.getRoot();
-
+        bottomSheetDialog = new BottomSheetDialog(getContext());
         return view;
     }
 
@@ -60,9 +66,49 @@ private static final String TAG= WorkerCompilationFormOneFragment.class.getSimpl
 
         binding.btnComplaintNext.setOnClickListener(view1 -> {
 
-            workerCompilationFormTwoViewModel.getConstruct(binding.edNuFacility.getText().toString()).observe(getViewLifecycleOwner(), constructByNameObserver);
 
-            //mListener.onFragmentInteraction(21);
+            mListener.onFragmentInteraction(21);
+        });
+
+
+        binding.edNuFacility.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetSearshList = new BottomSheetSearshList(getActivity(), bottomSheetDialog, new BottomSheetSearsh.bottomSheetSearsh() {
+                    @Override
+                    public void searshByNumber(String num_facility) {
+
+                          workerCompilationFormTwoViewModel.getConstruct(binding.edNuFacility.getText().toString()).observe(getViewLifecycleOwner(), constructByNameObserver);
+
+                    }
+                });
+
+                bottomSheetSearshList.openDialog("sohaib");
+            }
+        });
+
+        binding.edNuFacility.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if (i2 == 3) {
+
+                    Toast.makeText(getActivity(), "you can do it the request now ", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
         });
 
 
